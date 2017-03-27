391b8e083f4931bbd8cccd17046584b14fa71412
yinjiawind <1439954398@qq.com>
2017-03-27 21:23:49 +0800

    add thread pool

--- wsgi_server.py
+++ wsgi_server.py
@@ -3,68 +3,165 @@ import socket
 import select
 import StringIO
 import datetime
+import threading
+import time
+import Queue
 
 import config
 
-class WSGIServer(object):
-    def __init__(self, server_address, application):
-        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.serversocket.bind(server_address)
-        self.serversocket.listen(config.LISTEN_SIZE)
-        self.serversocket.setblocking(0)
-        self.serversocket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        host, port = self.serversocket.getsockname()[:2]
-        self.epoll = select.epoll()
-        self.epoll.register(self.serversocket.fileno(), select.EPOLLIN)
-        self.host = host
-        self.port = port
-        self.application = application
-        self.connections = {}
-        self.request_data = {}
-        self.response_data = {}
+_SHUTDOWNREQUEST = None
 
-    def serve_forever(self):
+
+class WorkerThread(threading.Thread):
+    """Thread which continuously polls a Queue for Connection objects.
+
+    Due to the timing issues of polling a Queue, a WorkerThread does not
+    check its own 'ready' flag after it has started. To stop the thread,
+    it is necessary to stick a _SHUTDOWNREQUEST object onto the Queue
+    (one for each running WorkerThread).
+    """
+
+    server = None
+    """The HTTP Server which spawned this thread, and which owns the
+    Queue and is placing active connections into it."""
+
+    ready = False
+    """A simple flag for the calling server to know when this thread
+    has begun polling the Queue."""
+
+    def __init__(self, server):
+        self.ready = False
+        self.server = server
+        threading.Thread.__init__(self)
+
+    def run(self):
         try:
+            self.ready = True
             while True:
-                events = self.epoll.poll(1)
-                for fileno, event in events:
-                    if fileno == self.serversocket.fileno():
-                        connection, address = self.serversocket.accept()
-                        connection.setblocking(0)
-                        self.epoll.register(connection.fileno(), select.EPOLLIN)
-                        self.connections[connection.fileno()] = connection
-                        self.request_data[connection.fileno()] = b''
-                        self.response_data[connection.fileno()] = b''
-                    elif event & select.EPOLLIN:
-                        data = self.connections[fileno].recv(1024)
-                        self.request_data[fileno] += data
-                        if len(data) != 1024:
-                            self.handle_request(fileno)
-                            del self.request_data[fileno]
-                            self.epoll.modify(fileno, select.EPOLLOUT)
-                    elif event & select.EPOLLOUT:
-                        byteswritten = self.connections[fileno].send(self.response_data[fileno])
-                        self.response_data[fileno] = self.response_data[fileno][byteswritten:]
-                        if len(self.response_data[fileno]) == 0:
-                            self.epoll.modify(fileno, 0)
-                            self.connections[fileno].shutdown(socket.SHUT_RDWR)
-                    elif event & select.EPOLLHUP:
-                        self.epoll.unregister(fileno)
-                        self.connections[fileno].close()
-                        del self.connections[fileno]
-        finally:
-            self.epoll.unregister(self.serversocket.fileno())
-            self.epoll.close()
-            self.serversocket.close()
+                conn = self.server.requests.get()
+                if conn is _SHUTDOWNREQUEST:
+                    return
 
-    def handle_request(self, fileno):
-        self.request_lines = self.request_data[fileno].splitlines()
+                conn.handle_request()
+        except (KeyboardInterrupt, SystemExit), exc:
+            self.server.interrupt = exc
+
+
+class ThreadPool(object):
+    """A Request Queue for the CherryPyWSGIServer which pools threads.
+
+    ThreadPool objects must provide min, get(), put(obj), start()
+    and stop(timeout) attributes.
+    """
+
+    def __init__(self, server, min=10, max=-1):
+        self.server = server
+        self.min = min
+        self.max = max
+        self._threads = []
+        self._queue = Queue.Queue()
+        self.get = self._queue.get
+
+    def start(self):
+        """Start the pool of threads."""
+        for i in range(self.min):
+            self._threads.append(WorkerThread(self.server))
+        for worker in self._threads:
+            worker.setName("CP Server " + worker.getName())
+            worker.start()
+        for worker in self._threads:
+            while not worker.ready:
+                time.sleep(.1)
+
+    def _get_idle(self):
+        """Number of worker threads which are idle. Read-only."""
+        return len([t for t in self._threads if t.conn is None])
+
+    idle = property(_get_idle, doc=_get_idle.__doc__)
+
+    def put(self, obj):
+        self._queue.put(obj)
+        if obj is _SHUTDOWNREQUEST:
+            return
+
+    def grow(self, amount):
+        """Spawn new worker threads (not above self.max)."""
+        for i in range(amount):
+            if self.max > 0 and len(self._threads) >= self.max:
+                break
+            worker = WorkerThread(self.server)
+            worker.setName("CP Server " + worker.getName())
+            self._threads.append(worker)
+            worker.start()
+
+    def shrink(self, amount):
+        """Kill off worker threads (not below self.min)."""
+        # Grow/shrink the pool if necessary.
+        # Remove any dead threads from our list
+        for t in self._threads:
+            if not t.isAlive():
+                self._threads.remove(t)
+                amount -= 1
+
+        if amount > 0:
+            for i in range(min(amount, len(self._threads) - self.min)):
+                # Put a number of shutdown requests on the queue equal
+                # to 'amount'. Once each of those is processed by a worker,
+                # that worker will terminate and be culled from our list
+                # in self.put.
+                self._queue.put(_SHUTDOWNREQUEST)
+
+    def stop(self, timeout=5):
+        # Must shut down threads here so the code that calls
+        # this method can know when all threads are stopped.
+        for worker in self._threads:
+            self._queue.put(_SHUTDOWNREQUEST)
+
+        # Don't join currentThread (when stop is called inside a request).
+        current = threading.currentThread()
+        if timeout and timeout >= 0:
+            endtime = time.time() + timeout
+        while self._threads:
+            worker = self._threads.pop()
+            if worker is not current and worker.isAlive():
+                try:
+                    if timeout is None or timeout < 0:
+                        worker.join()
+                    else:
+                        remaining_time = endtime - time.time()
+                        if remaining_time > 0:
+                            worker.join(remaining_time)
+                        if worker.isAlive():
+                            # We exhausted the timeout.
+                            # Forcibly shut down the socket.
+                            c = worker.conn
+                            if c and not c.rfile.closed:
+                                try:
+                                    c.socket.shutdown(socket.SHUT_RD)
+                                except TypeError:
+                                    # pyOpenSSL sockets don't take an arg
+                                    c.socket.shutdown()
+                            worker.join()
+                except (AssertionError,
+                        # Ignore repeated Ctrl-C.
+                        # See http://www.cherrypy.org/ticket/691.
+                        KeyboardInterrupt), exc1:
+                    pass
+
+class Connection(object):
+    def __init__(self, server, fileno):
+        self.server = server
+        self._fileno = fileno
+
+    def handle_request(self):
+        self.request_lines = self.server.request_data[self._fileno].splitlines()
         try:
             self.get_url_parameter()
             env = self.get_environ()
-            app_data = self.application(env, self.start_response)
-            self.response_data[fileno] = self.gen_response_data(app_data)
+            app_data = self.server.application(env, self.server.start_response)
+            self.server.response_data[self._fileno] = self.gen_response_data(app_data)
+            del self.server.request_data[self._fileno]
+            self.server.epoll.modify(self._fileno, select.EPOLLOUT)
             print '[{0}] "{1}" {2}'.format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                                            self.request_lines[0], self.status)
         except Exception, e:
@@ -77,7 +174,7 @@ class WSGIServer(object):
                 self.request_dict[item.split(':')[0]] = item.split(':')[1]
             else:
                 break
-        self.post_data = "\r\n".join(self.request_lines[index+1:])
+        self.post_data = "\r\n".join(self.request_lines[index + 1:])
         self.request_method, self.path, self.request_version = self.request_dict.get('Path').split()
         self.query_string = ""
         self.path_parse()
@@ -99,12 +196,74 @@ class WSGIServer(object):
             'wsgi.run_once': False,
             'REQUEST_METHOD': self.request_method,
             'PATH_INFO': self.path,
-            'SERVER_NAME': self.host,
-            'SERVER_PORT': self.port,
+            'SERVER_NAME': self.server.host,
+            'SERVER_PORT': self.server.port,
             'USER_AGENT': self.request_dict.get('User-Agent'),
             'QUERY_STRING': self.query_string
         }
         return env
+
+    def gen_response_data(self, app_data):
+        response = 'HTTP/1.1 {status}\r\n'.format(status=self.server.status)
+        for header in self.server.headers:
+            response += '{0}: {1}\r\n'.format(*header)
+        response += '\r\n'
+        for data in app_data:
+            response += data
+        return response
+
+class WSGIServer(object):
+    def __init__(self, server_address, application):
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.serversocket.bind(server_address)
+        self.serversocket.listen(config.LISTEN_SIZE)
+        self.serversocket.setblocking(0)
+        self.serversocket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        host, port = self.serversocket.getsockname()[:2]
+        self.epoll = select.epoll()
+        self.epoll.register(self.serversocket.fileno(), select.EPOLLIN)
+        self.host = host
+        self.port = port
+        self.application = application
+        self.connections = {}
+        self.request_data = {}
+        self.response_data = {}
+        self.requests = ThreadPool(self)
+
+    def serve_forever(self):
+        try:
+            self.requests.start()
+            while True:
+                events = self.epoll.poll(1)
+                for fileno, event in events:
+                    if fileno == self.serversocket.fileno():
+                        connection, address = self.serversocket.accept()
+                        connection.setblocking(0)
+                        self.epoll.register(connection.fileno(), select.EPOLLIN)
+                        self.connections[connection.fileno()] = connection
+                        self.request_data[connection.fileno()] = b''
+                        self.response_data[connection.fileno()] = b''
+                    elif event & select.EPOLLIN:
+                        data = self.connections[fileno].recv(1024)
+                        self.request_data[fileno] += data
+                        if len(data) != 1024:
+                            conn = Connection(self, fileno)
+                            self.requests.put(conn)
+                    elif event & select.EPOLLOUT:
+                        byteswritten = self.connections[fileno].send(self.response_data[fileno])
+                        self.response_data[fileno] = self.response_data[fileno][byteswritten:]
+                        if len(self.response_data[fileno]) == 0:
+                            self.epoll.modify(fileno, 0)
+                            self.connections[fileno].shutdown(socket.SHUT_RDWR)
+                    elif event & select.EPOLLHUP:
+                        self.epoll.unregister(fileno)
+                        self.connections[fileno].close()
+                        del self.connections[fileno]
+        finally:
+            self.epoll.unregister(self.serversocket.fileno())
+            self.epoll.close()
+            self.serversocket.close()
 
     def start_response(self, status, response_headers):
         headers = [
@@ -113,15 +272,6 @@ class WSGIServer(object):
         ]
         self.headers = response_headers + headers
         self.status = status
-
-    def gen_response_data(self, app_data):
-        response = 'HTTP/1.1 {status}\r\n'.format(status=self.status)
-        for header in self.headers:
-            response += '{0}: {1}\r\n'.format(*header)
-        response += '\r\n'
-        for data in app_data:
-            response += data
-        return response
 
 def application(environ, start_response):
     status = '200 OK'
